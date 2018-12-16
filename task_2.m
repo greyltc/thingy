4ff3bfd744e44d84e84914aef0bd2ce13f13bf09
Grey Christoforo <grey@christoforo.net>
2018-12-16 17:50:32 +0000

    added unity sum check for weights

--- task_2.m
+++ task_2.m
@@ -9,6 +9,9 @@ weights = [
     0.20; % Shawbury
     0.13];% Waddington
 
+% check that weights add to one
+assert(sum(weights) == 1)
+
 % read in temperature data into table
 IN = readtable('data/UK-Temperatures.csv');
 TT = table2timetable(IN); % convert to timetable
@@ -18,4 +21,4 @@ TT.Properties.DimensionNames(1) = {'Date'}; % change time header because OCD
 filled = fillmissing(TT,'linear'); % use linear interpolation (& extrapolation) to replace missing data
 filled.Weighted = filled.Variables*weights; % calculate weighted averages for each row in the table
 dailyMeansTT = retime(filled, 'daily', 'mean'); % Calculate the mean for each reading over daily time bins
-dailyMeansTT(1:5,end) % print the first five rows of the solution table
+dailyMeansTT(1:5,end) % print the first five rows of the last column (the weighted average) of the solution table
