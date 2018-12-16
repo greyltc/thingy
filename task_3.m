d041bf96e8711bff0865a2e5d32e50ec25c7dbf2
Grey Christoforo <grey@christoforo.net>
2018-12-16 22:44:31 +0000

    make task 3 great again

--- task_3.m
+++ task_3.m
@@ -1,50 +1,111 @@
-function task_3
+clear all
 
-IN_temp = readtable('Temperatures-South-Carolina.csv');
-IN_demand = readtable('Residential-Demand-South-Carolina.csv');
+IN_temp = readtable('data/Temperatures-South-Carolina.csv');
+IN_demand = readtable('data/Residential-Demand-South-Carolina.csv');
 
-dates_dem = IN_demand{:,1};
-demand = IN_demand{:,2};
-[yd,md] = ymd(dates_dem);
-yd = string(yd);
-yd(strlength(yd)==1) = strcat('0',yd(strlength(yd)==1));
-md =  string(md);
-md(strlength(md)==1) = strcat('0',md(strlength(md)==1));
-Dd = strcat(yd,'/',md);
-u_d_d = unique(Dd);
+tempTT = table2timetable(IN_temp);
+demandTT = table2timetable(IN_demand);
 
-dates_temp = IN_temp{:,1};
-temperatures = IN_temp{:,2};
+% define time ranges for splitting up the dataset
+TR2013 = timerange('13-01-01','14-01-01');
+TR2014 = timerange('14-01-01','15-01-01');
+TR2015 = timerange('15-01-01','16-01-01');
+TR2016 = timerange('16-01-01','17-01-01');
+TR2017 = timerange('17-01-01','18-01-01');
 
-[y,m] = ymd(dates_temp);
-y = string(y);
-y(strlength(y)==1) = strcat('0',y(strlength(y)==1));
-m =  string(m);
-m(strlength(m)==1) = strcat('0',m(strlength(m)==1));
+% split the big time data set up by year
+tempTT2013 = tempTT(TR2013,:);
+tempTT2014 = tempTT(TR2014,:);
+tempTT2015 = tempTT(TR2015,:);
+tempTT2016 = tempTT(TR2016,:);
+tempTT2017 = tempTT(TR2017,:);
 
-D = strcat(y,'/',m);
-u_d = unique(D);
+% shift each timeset to a common start time
+tempTT2013.Var1 = tempTT2013.Var1 - tempTT2013.Var1(1);
+tempTT2014.Var1 = tempTT2014.Var1 - tempTT2014.Var1(1);
+tempTT2015.Var1 = tempTT2015.Var1 - tempTT2015.Var1(1);
+tempTT2016.Var1 = tempTT2016.Var1 - tempTT2016.Var1(1);
+tempTT2017.Var1 = tempTT2017.Var1 - tempTT2017.Var1(1);
 
-means = nan([numel(u_d),size(temperatures,2)]);
-for i=1:numel(u_d)
-    id = strcmp(D,u_d(i));
-    means(i,:) = nanmean(temperatures(id,:),1);
-end
-overall_mean = [u_d,string(means)];
+% stack data sets on top of eachother tomake the analysis below possible
+masterT = [tempTT2013; tempTT2014; tempTT2015; tempTT2016; tempTT2017];
 
-[index_temp,index_dem] = ismember(u_d,u_d_d);
-data = [means(index_temp),demand(index_dem(index_dem>0))];
+% here's the input data to our temperature vs time model
+t = hours(masterT.Var1);
+y = masterT.Var2;
 
-p = polyfit(data(:,1),data(:,2),2);
+% here we define our model for the temperature over a year
+%found on the world wide web:
+%https://www.scipy-lectures.org/intro/scipy/auto_examples/solutions/plot_curvefit_temperature_data.html)
+hoursInLeapYear = 366*24;
+tmpModel = @(ampl,avg,time_offset,x) avg+ampl*cos((x+time_offset)*2*pi/hoursInLeapYear);
 
-plot(data(:,1),data(:,2),'x')
-hold on
-plot(linspace(min(data(:,1)),max(data(:,1)),1000),polyval(p,linspace(min(data(:,1)),max(data(:,1)),1000)))
-xlabel('Monthly Average Temperature')
-ylabel('Demand')
+% fit the data to our model
+fitobject = fit(t,y,tmpModel);
 
+% visualize the fit
+%figure
+%plot(fitobject,t,y)
 
-predictions = polyval(p,means(~index_temp)); % predictions for January-April 2017
+% these are the hours we need temperatures for (april 1 to may 1)
+hoursToAprilFools = hours(datetime('00-03-31') - datetime('00-01-01'));
+aprilHours = (0:24:(24*30)) + hoursToAprilFools;
 
-%% daily analysis
-demand_daily = polyval(p,temperatures)./(365/12); % daily demand for gas (based on average number of days in a month)
+% now we use the fit to our model to predict the april temperatures
+aprilTemperatures = feval(fitobject,aprilHours);
+
+days2017 = [hours(tempTT2017.Var1); aprilHours']/24;
+
+% stack our temp predictions onto the known temp data for 2017
+temps2017 = [tempTT2017.Var2; aprilTemperatures];
+
+% visualize the temperature prediction
+%figure
+%plot(days2017,temps2017,'-*')
+%xlabel('Days since start of 2017')
+%ylabel('Temperature [Deg F]')
+%grid on
+
+% second part: start to consider the demand data
+pre2017 = timerange('13-01-01','16-12-31'); %relevant period for demand data
+pre2017TT = tempTT(pre2017,:); % pick out the temp data over this range
+
+% combine temperature and demand data converting demand to a daily sampling
+% interval by linear interpolation
+tempAndDemand = synchronize(demandTT,pre2017TT,'daily','linear');
+
+% here's the input data to our demand vs temperature model
+T = tempAndDemand.Var2_pre2017TT; % temperatures
+D = tempAndDemand.Var2_demandTT; % demands
+
+% fit the data to a first order exponential function: D = a*exp(b*T)
+% using the Least Absolute Residual robust linear least-squares fitting
+% method (found to give best results in interactive fitting GUI)
+fitobject2 = fit(T,D,'exp1','Robust','LAR');
+
+% visualize the fit
+%figure
+%plot(fitobject2,T,D)
+
+% use the fit to our exponential model to predict the daily demand given
+% the temerature data for the first four months of 2017
+demand2017 = feval(fitobject2,temps2017);
+
+% figure out the dates that go along with the 2017 demand data we predicted
+dates2017 = days(days2017) + datetime('17-01-01');
+
+% form a timetable filled with the results of our analysis
+resultTT = timetable(dates2017);
+resultTT.Demand = demand2017;
+
+% print the first five rows (days) of the answer table
+resultTT(1:5,:)
+
+%           dates2017         Demand
+%     ____________________    ______
+% 
+%     01-Jan-0017 00:00:00    2599.9
+%     02-Jan-0017 00:00:00    1473.3
+%     03-Jan-0017 00:00:00    1726.2
+%     04-Jan-0017 00:00:00    3212.5
+%     05-Jan-0017 00:00:00    3473.9
