7aa9adf0b9c427e6fc42c8f08e09c07b771b2e0a
Grey Christoforo <grey@christoforo.net>
2018-12-16 17:38:21 +0000

    add comments to task 2 and add data files

--- task_2.m
+++ task_2.m
@@ -1,18 +1,21 @@
 function task_2
+
+% weights for weighted average these MUST sum to unity
 weights = [
-    0.14; % Brce Norton
+    0.14; % Brice Norton
     0.10; % Herstmonceux
     0.30; % Heathrow
     0.13; % Nottingham
     0.20; % Shawbury
     0.13];% Waddington
 
-IN = readtable('UK-Temperatures.csv');
-TT = table2timetable(IN);
+% read in temperature data into table
+IN = readtable('data/UK-Temperatures.csv');
+TT = table2timetable(IN); % convert to timetable
 
-TT.Properties.DimensionNames(1) = {'Date'};
+TT.Properties.DimensionNames(1) = {'Date'}; % change time header because OCD
 
-filled = fillmissing(TT,'linear');
-filled.Weighted = filled.Variables*weights;
-dailyMeansTT = retime(filled, 'daily', 'mean');
-dailyMeansTT(1:5,end)
+filled = fillmissing(TT,'linear'); % use linear interpolation (& extrapolation) to replace missing data
+filled.Weighted = filled.Variables*weights; % calculate weighted averages for each row in the table
+dailyMeansTT = retime(filled, 'daily', 'mean'); % Calculate the mean for each reading over daily time bins
+dailyMeansTT(1:5,end) % print the first five rows of the solution table
